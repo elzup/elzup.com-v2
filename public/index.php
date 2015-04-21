7fcd273b7f11a0bfece43022adc98e7914320f89
elzzup <guild0105@gmail.com>
2015-04-21 20:16:59 +0900

    update toppage add sns panels

--- public/index.php
+++ public/index.php
@@ -13,6 +13,7 @@
 	<script src="./bower_components/webcomponentsjs/webcomponents.min.js"></script>
 	<link rel="import" href="./bower_components/polymer/polymer.html">
 	<link rel="import" href="./elements/top-panel">
+	<link rel="import" href="./elements/toplink-panel">
 </head>
 <?php
 
@@ -26,14 +27,43 @@ require_once('./helpers/setup_productions.php');
 <div id="wrapper">
 	<h1 id="top-title">elzup.com</h1>
 
-	<nav class="flex">
+	<nav class="flex top">
 		<top-panel name="Profile" href="profile"></top-panel>
 		<top-panel name="Products" href="products"></top-panel>
 		<top-panel name="Activity" href="activity"></top-panel>
-		<top-panel href="#"></top-panel>
+		<div id="links" class="flex">
+			<toplink-panel
+				name="Twitter"
+				href="https://twitter.com/Arzzup"
+				color="#55acee"
+				></toplink-panel>
+			<toplink-panel
+				name="Facebook"
+				href="https://www.facebook.com/takahashiroto"
+				color="#3b5998"
+				></toplink-panel>
+			<toplink-panel
+				name="Github"
+				href="https://github.com/elzzup"
+				color="#555"
+				></toplink-panel>
+			<toplink-panel
+				name="Qiita"
+				href="http://twitter.com/arzzup"
+				color="#64c914"
+				></toplink-panel>
+			<toplink-panel
+				name="Hatena"
+				href="http://twitter.com/arzzup"
+				color="#F26C68"
+				></toplink-panel>
+			<toplink-panel
+				name="Tumblr"
+				href="http://twitter.com/arzzup"
+				color="#34526f"
+				></toplink-panel>
+		</div>
 	</nav>
-	<foorter class="flex">
-	</foorter>
 </div>
 
 </body>
